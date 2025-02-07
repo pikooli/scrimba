169fa98df8a5773c67befe7561223d12e86d0ead
pascal Zhang <panamepoul@gmail.com>
2025-02-07 15:42:21 +0100

    translation

--- huggingface/src/app/page.tsx
+++ huggingface/src/app/page.tsx
@@ -4,11 +4,9 @@ import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { HfInference } from '@huggingface/inference';
 
-// const model = 'distilbert-base-uncased-finetuned-sst-2-english'; // only score 2 classes
-const model = 'SamLowe/roberta-base-go_emotions'; // score 15 classes
+const model = 'facebook/mbart-large-50-many-to-many-mmt'; // score 15 classes
 
-const textToClassify =
-  "I just bought a new camera. It's the best camera I've ever owned!";
+const textToTranslate = "It's an exciting time to be an AI engineer";
 
 export default function Home() {
   const [result, setResult] = useState<string>('');
@@ -16,9 +14,13 @@ export default function Home() {
   useEffect(() => {
     const inference = new HfInference(process.env.NEXT_PUBLIC_HF_API_KEY);
     inference
-      .textClassification({
+      .translation({
         model,
-        inputs: textToClassify,
+        inputs: textToTranslate,
+        parameters: {
+          src_lang: 'en_XX',
+          tgt_lang: 'fr_XX',
+        },
       })
       .then((result) => {
         setResult(JSON.stringify(result));
@@ -30,10 +32,10 @@ export default function Home() {
     <div>
       <Image src="/huggingFace.svg" alt="Hugging Face" width={95} height={88} />
       <p className="rounded-lg bg-gray-100 p-4 text-lg font-medium text-gray-800">
-        Text to classify: {textToClassify}
+        Text to translate: {textToTranslate}
       </p>
       <p className="rounded-lg bg-gray-100 p-4 text-lg font-medium text-gray-800">
-        classification: {result}
+        translation: {result}
       </p>
     </div>
   );
