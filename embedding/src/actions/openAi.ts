f034c1a6fee59a0731a4f1ec5f2fc7fc417e0c9b
pascal Zhang <panamepoul@gmail.com>
2025-02-21 00:06:06 +0100

    adding supabase

--- embedding/src/actions/openAi.ts
+++ embedding/src/actions/openAi.ts
@@ -1,5 +1,11 @@
 'use server';
 import OpenAI from 'openai';
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_API_KEY
+);
 
 const openai = new OpenAI({
   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
@@ -8,11 +14,16 @@ const openai = new OpenAI({
 const model = 'text-embedding-ada-002';
 
 const data = [
-  "Beyond Mars: speculating life on distant planets.",
-  "Jazz under stars: a night in New Orleans' music scene.",
-  "Mysteries of the deep: exploring uncharted ocean caves.",
-  "Rediscovering lost melodies: the rebirth of vinyl culture.",
-  "Tales from the tech frontier: decoding AI ethics.",
+  "Beyond Mars (1 hr 15 min): Join space enthusiasts as they speculate about extraterrestrial life and the mysteries of distant planets.",
+  "Jazz under stars (55 min): Experience a captivating night in New Orleans, where jazz melodies echo under the moonlit sky.",
+  "Mysteries of the deep (1 hr 30 min): Dive with marine explorers into the uncharted caves of our oceans and uncover their hidden wonders.",
+  "Rediscovering lost melodies (48 min): Journey through time to explore the resurgence of vinyl culture and its timeless appeal.",
+  "Tales from the tech frontier (1 hr 5 min): Navigate the complex terrain of AI ethics, understanding its implications and challenges.",
+  "The soundscape of silence (30 min): Traverse the globe with sonic explorers to find the world's most serene and silent spots.",
+  "Decoding dreams (1 hr 22 min): Step into the realm of the subconscious, deciphering the intricate narratives woven by our dreams.",
+  "Time capsules (50 min): Revel in the bizarre, endearing, and profound discoveries that unveil the quirks of a century past.",
+  "Frozen in time (1 hr 40 min): Embark on an icy expedition, unearthing secrets hidden within the majestic ancient glaciers.",
+  "Songs of the Sea (1 hr): Dive deep with marine biologists to understand the intricate whale songs echoing in our vast oceans."
 ]; 
 
 export const createEmbedding = async () => {
@@ -34,7 +45,13 @@ export const createEmbedding = async () => {
           ?.embedding,
       };
     });
-    console.log('hash', hash);
+    const documents = hash.map((item) => {
+      return {
+        content: item.text,
+        embedding: item.embedding,
+      };
+    });
+    await supabase.from('documents').insert(documents);
     return hash;
   } catch (err) {
     console.error('error: ', err);
