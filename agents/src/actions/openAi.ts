c8390865f7231e3966501f341e12a93baf665e2b
pascal Zhang <panamepoul@gmail.com>
2025-02-24 22:35:19 +0100

    function auto calling

--- agents/src/actions/openAi.ts
+++ agents/src/actions/openAi.ts
@@ -1,7 +1,7 @@
 'use server';
 import OpenAI from 'openai';
 import { ChatCompletionMessageParam } from 'openai/resources/chat/completions';
-import { getCurrentWeather, getLocation, tools } from '@/tools';
+import { tools } from '@/tools';
 
 const openai = new OpenAI({
   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
@@ -12,57 +12,25 @@ const model = 'gpt-3.5-turbo';
 export const chatCompletion = async (
   messages: ChatCompletionMessageParam[]
 ) => {
-  const response = await openai.chat.completions.create({
-    model,
-    messages: messages,
-    tools: tools,
-  });
-  console.log('response', JSON.stringify(response));
-  return response;
+  const runner = openai.beta.chat.completions
+    .runTools({
+      model,
+      messages: messages,
+      tools: tools,
+    })
+    .on('totalUsage', (usage) => {
+      console.log('usage', JSON.stringify(usage));
+    })
+    .on('message', (message) => {
+      console.log('message', JSON.stringify(message));
+    });
+  const finalContent = await runner.finalContent();
+  console.log('finalContent', JSON.stringify(finalContent));
+  return finalContent;
 };
 
-const invokeTools = async (actionName: string, parameters: any) => {
-  if (actionName === 'getCurrentWeather') {
-    return await getCurrentWeather(parameters);
-  }
-  if (actionName === 'getLocation') {
-    return await getLocation();
-  }
-  return null;
-};
-
-export const agent = async (
-  messages: ChatCompletionMessageParam[]
-): Promise<string> => {
+export const agent = async (messages: ChatCompletionMessageParam[]) => {
   const response = await chatCompletion(messages);
-  if (response.choices[0].message.tool_calls?.length) {
-    const newMessages: ChatCompletionMessageParam[] = [
-      ...messages,
-      response.choices[0].message,
-    ];
-    const toolCallsPromises = response.choices[0].message.tool_calls?.map(
-      async (toolCall) => {
-        console.log('toolCall =========', toolCall);
-        const toolResponse = await invokeTools(
-          toolCall.function.name,
-          JSON.parse(toolCall.function.arguments)
-        );
-        console.log('toolResponse =========', toolResponse);
-        if (toolResponse) {
-          newMessages.push({
-            role: 'tool',
-            content: toolResponse,
-            tool_call_id: toolCall.id,
-          });
-        }
-      }
-    );
-    await Promise.all(toolCallsPromises);
-    const responseAfterTool = await agent(newMessages);
-    console.log('responseAfterTool =========', responseAfterTool);
-    return responseAfterTool;
-  }
-
   console.log('response =========', JSON.stringify(response));
-  return response.choices[0].message.content ?? '';
+  return response;
 };
--- agents/src/tools.ts
+++ agents/src/tools.ts
@@ -1,16 +1,8 @@
-import { ChatCompletionTool } from 'openai/resources/chat/completions';
-export async function getCurrentWeather({
-  location,
-  unit = 'fahrenheit',
-}: {
-  location: string;
-  unit: string;
-}) {
+export async function getCurrentWeather({ location }: { location: string }) {
   console.log('location =========', location);
-  console.log('unit =========', unit);
   const weather = {
-    temperature: unit === 'fahrenheit' ? '72' : '22',
-    unit: unit === 'fahrenheit' ? 'F' : 'C',
+    temperature: '72',
+    unit: 'F',
     forecast: location === 'New York City' ? 'sunny' : 'cloudy',
   };
   return JSON.stringify(weather);
@@ -20,22 +12,18 @@ export async function getLocation() {
   return 'New York City, NY';
 }
 
-export const tools: ChatCompletionTool[] = [
+export const tools: any[] = [
   {
     type: 'function',
     function: {
-      name: 'getCurrentWeather',
-      description: 'Get the current weather in a location',
+      function: getCurrentWeather,
+      parse: JSON.parse,
       parameters: {
         type: 'object',
         properties: {
           location: {
             type: 'string',
             description: 'The location from where to get the weather',
-          },
-          unit: {
-            type: 'string',
-            enum: ['celcius', 'fahrenheit'],
           },
         },
         required: ['location'],
@@ -45,7 +33,13 @@ export const tools: ChatCompletionTool[] = [
   {
     type: 'function',
     function: {
-      name: 'getLocation',
+      function: getLocation,
+      parse: JSON.parse,
+      parameters: {
+        type: 'object',
+        properties: {},
+        required: [],
+      },
     },
   },
 ];
