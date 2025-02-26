746bdc8bb35375f726ed6f79106cc799cff62441
pascal Zhang <panamepoul@gmail.com>
2025-02-26 16:07:34 +0100

    inpaint

--- multimodality/src/actions/openAi.ts
+++ multimodality/src/actions/openAi.ts
@@ -1,24 +1,21 @@
 'use server';
 import OpenAI from 'openai';
+import fs from 'fs';
+import path from 'path';
 
 const openai = new OpenAI({
   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 });
 
-const model = 'dall-e-3';
-// const model = 'dall-e-2';
-
 export const generateImage = async (
   prompt: string
 ) => {
-  const response = await openai.images.generate({
-    model,
-    prompt,
-    n: 1,
-    size: '1024x1024',
-    response_format: "b64_json",
-    quality: "hd",
-    style: "vivid"
+  console.log('prompt',prompt)
+  const response = await openai.images.edit({
+      image: fs.createReadStream(path.join(process.cwd(), 'public', 'building.png')),
+      mask: fs.createReadStream(path.join(process.cwd(), 'public', 'mask.png')),
+      prompt,
+      response_format: "b64_json",
   });
   console.log('response',response)
   return `data:image/png;base64,${response.data[0].b64_json}`;
